b92507fdcbed2c11715560391f64dca50608d07e
Krzysiek Szularz <szuliq@gmail.com>
2013-08-02 11:46:50 +0200

    Don't hack around a problem

--- aldryn_news/utils.py
+++ aldryn_news/utils.py
@@ -1,19 +1,7 @@
 # -*- coding: utf-8 -*-
-from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
 
-
-class FormatableReverse(object):
-    """Reverses view name at a time of string formatting.
-
-    Trims the dict used to formatting to desired keys only.
-
-    Usefull as a `redirect_to` `url` parameter. """
-
-    def __init__(self, viewname, keys=None):
-        self.viewname = viewname
-        self.keys = keys
-
-    def __mod__(self, kwargs):
-        in_keys = lambda (x, y): x in self.keys
-        kwargs = dict(filter(in_keys, kwargs.iteritems()))
-        return reverse(self.viewname, kwargs=kwargs)
+def redirect_to_viewname(request, viewname, keys, **kwargs):
+    in_keys = lambda (x, y): x in keys
+    kwargs = dict(filter(in_keys, kwargs.iteritems()))
+    return redirect(viewname, **kwargs)
