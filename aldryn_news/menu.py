957dc2f359d4de9b4d4761c59610bf73ac2ce59c
Benjamin Wohlwend <piquadrat@gmail.com>
2013-11-20 09:53:38 +0100

    don't take down the whole site if a category URL isn't reversable
    this also helps on multisite setups, where the app is not neceseraly
    running on all sites

--- aldryn_news/menu.py
+++ aldryn_news/menu.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import NoReverseMatch
 from django.db.models.signals import post_save, post_delete
+from django.utils.translation import ugettext_lazy as _
 
 from aldryn_news.models import Category
 
@@ -19,10 +20,13 @@ class NewsCategoryMenu(CMSAttachMenu):
         # bug in hvad - Meta ordering isn't preserved
         categories = categories.order_by('ordering')
         for category in categories:
-            node = NavigationNode(category.name,
-                                  category.get_absolute_url(),
-                                  category.slug)
-            nodes.append(node)
+            try:
+                node = NavigationNode(category.name,
+                                      category.get_absolute_url(),
+                                      category.slug)
+                nodes.append(node)
+            except NoReverseMatch:
+                pass
         return nodes
 
 menu_pool.register_menu(NewsCategoryMenu)
