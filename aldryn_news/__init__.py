4e848f0251e9db30a9df47a302272991296c74bf
Patrick Lauber <digi@treepy.com>
2014-07-28 17:34:50 +0200

    limit to positive integer

--- aldryn_news/__init__.py
+++ aldryn_news/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.1.14'
+__version__ = '0.1.15'
 request_news_identifier = 'aldryn_news_current_news'
--- aldryn_news/models.py
+++ aldryn_news/models.py
@@ -206,7 +206,7 @@ class LatestNewsPlugin(CMSPlugin):
         (SIMPLE, _("Simple list")),
     )
 
-    latest_entries = models.IntegerField(default=5, help_text=_('The number of latests entries to be displayed.'))
+    latest_entries = models.PositiveSmallIntegerField(default=5, help_text=_('The number of latests entries to be displayed.'))
     type_list = models.CharField(verbose_name=_("Type of list"), choices=TYPES, default=FULL, max_length=255)
     tags = models.ManyToManyField('taggit.Tag', blank=True, help_text=_('Show only the news tagged with chosen tags.'))
 
